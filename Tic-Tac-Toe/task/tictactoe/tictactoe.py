43afead171e009a89ba55066e7cdc491c681f93a
xodj <xo.dj@ya.ru>
2021-02-09 16:53:41 +0300

    add frame and dynamics

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,4 +1,10 @@
 # write your code here
-print("""X O X
-O X O
-X X O""")
+def display_string(cells_list):
+    return """---------
+| """ + cells_list[0] + """ """ + cells_list[1] + """ """ + cells_list[2] + """ |
+| """ + cells_list[3] + """ """ + cells_list[4] + """ """ + cells_list[5] + """ |
+| """ + cells_list[6] + """ """ + cells_list[7] + """ """ + cells_list[8] + """ |
+---------"""
+
+
+print(display_string(list(input('Enter cells: '))))
